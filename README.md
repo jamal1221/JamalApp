46e661842c83f0637beaf8baf846bb1a2cccff78
jamal1221 <130873278+jamal1221@users.noreply.github.com>
2025-01-12 00:18:36 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# JamalApp
+#  Projet Transformation Digital
+
+Ce projet de transformation digitale a été réalisé par Zakaria Belrhayal et Ibtissam LABYADY, encadrés par M. Senouni Amine. L'objectif de ce projet était d'optimiser les ventes d'une entreprise en utilisant des outils de visualisation de données tels que Power BI, des techniques de machine learning pour prédire les ventes futures, ainsi que des solutions basées sur le cloud et une application mobile.
+
+## Contexte
+Ce projet se concentre sur la gestion du stock dans une entreprise commerciale. La gestion efficace du stock est essentielle pour maximiser les ventes et réduire les coûts liés à la surstockage ou aux ruptures de stock.
+
+## Problématique
+La problématique principale abordée dans ce projet est la suivante : Comment une entreprise peut-elle optimiser ses ventes en utilisant des outils de visualisation de données tels que Power BI et des techniques de machine learning pour prédire les ventes futures ?
+
+## Solutions proposées
+Les solutions mises en place dans ce projet sont les suivantes :
+
+1. Analyse des données avec Power BI : Utilisation de Power BI pour visualiser les données de vente, identifier les tendances, et prendre des décisions éclairées basées sur ces informations.
+   ![image](https://github.com/ibtissam01/Digital-Transformation/assets/89752387/0dbc9794-7a1d-4b0b-b268-8c7d419c1b1b)
+
+
+3. Cloud Microsoft Azure : Utilisation du cloud Microsoft Azure pour le stockage sécurisé des données et l'accès aux ressources informatiques à la demande.
+   ![image](https://github.com/ibtissam01/Digital-Transformation/assets/89752387/765f65ba-d7f2-4339-8032-5305bc859fe3)
+
+
+5. Machine Learning - Régression : Utilisation de techniques de machine learning, en particulier la régression, pour prédire les ventes futures en analysant les données historiques.
+   ![image](https://github.com/ibtissam01/Digital-Transformation/assets/89752387/670fa096-aabc-4e45-a096-52c07b9bc32f)
+
+
+6. Solution mobile avec Streamlit : Développement d'une application mobile conviviale à l'aide de Streamlit, permettant aux utilisateurs d'accéder facilement aux informations de vente et de stock depuis n'importe où.
+   ![image](https://github.com/ibtissam01/Digital-Transformation/assets/89752387/4acdf760-5051-426f-a8e9-506e26e13dfd)
+
+   [Streamlit App](ibtissam01-stocka-main-zty64w.streamlit.app)
+
+## Accès à l'application
+Vous pouvez accéder à notre application via le lien suivant : [Streamlit App](ibtissam01-stocka-main-zty64w.streamlit.app)
+
+Zakaria Belrhayal
+Ibtissam LABYADY
---
+++ main.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+# Charger les données
+data = pd.read_csv("merged_data.csv")
+
+# Afficher les données brutes
+st.header("Données brutes")
+st.write(data.head())
+
+
+# Afficher des informations sur les données
+st.write('Informations sur les données')
+st.write(data.info())
+
+# Identifier les valeurs aberrantes
+st.subheader('Identifier les valeurs aberrantes')
+plt.figure(figsize=(10,6))
+sns.boxplot(x='QUANTITY', data=data)
+st.pyplot()
+
+# Traiter les valeurs manquantes
+st.subheader('Traiter les valeurs manquantes')
+st.write(data.isnull().sum())
+
+# Résumé statistique des données
+st.header("Résumé statistique")
+st.write(data.describe())
+
+
+
+# Identifier les relations entre les variables
+st.subheader('Identifier les relations entre les variables')
+sns.pairplot(data[['QUANTITY','SELLING PRICE','revenue']])
+st.pyplot()
+
+st.write('Corrélation entre les variables')
+st.write(data[['QUANTITY','SELLING PRICE','revenue']].corr())
+
+# Visualiser les distributions devariables
+st.subheader('Visualiser les distributions de variables')
+plt.figure(figsize=(10,6))
+sns.histplot(data['SELLING PRICE'], bins=20)
+st.pyplot()
+
+# Identifier les tendances temporelles
+st.subheader('Identifier les tendances temporelles')
+data['DATE'] = pd.to_datetime(data['DATE'])
+data.set_index('DATE', inplace=True)
+
+plt.figure(figsize=(15,5))
+sns.lineplot(x=data.index, y='revenue', data=data)
+st.pyplot()
+
+# Identifier les catégories de produits les plus vendues
+st.subheader('Identifier les catégories de produits les plus vendues')
+plt.figure(figsize=(10,6))
+sns.countplot(x='CATEGORY', data=data)
+st.pyplot()
+
+# Identifier les modes de paiement les plus populaires
+st.subheader('Identifier les modes de paiement les plus populaires')
+plt.figure(figsize=(10,6))
+sns.countplot(x='PAYMENT MODE', data=data)
+st.pyplot()
+# Histogramme des ventes
+st.header("Histogramme des ventes")
+plt.hist(data["revenue"])
+st.pyplot()
+
+# Diagramme circulaire des catégories de produits
+st.header("Diagramme circulaire des catégories de produits")
+categories = data.groupby("CATEGORY")["revenue"].sum()
+plt.pie(categories, labels=categories.index)
+st.pyplot()
+image = Image.open('image.png')
+st.image(image)
+
